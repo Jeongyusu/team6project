85c603997227e87269f2ebd79937d7a6f5498132
Jeongyusu <onlys2545@naver.com>
2023-08-31 21:27:24 +0900

    [FIX] mustache 주석으로 인한 에러 수정]

--- src/main/java/shop/mtcoding/project/user/UserController.java
+++ src/main/java/shop/mtcoding/project/user/UserController.java
@@ -1,14 +1,8 @@
 package shop.mtcoding.project.user;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
-
-    @GetMapping("/")
-    public String home() {
-        return "user/user_resume_write";
-    }
 
 }
