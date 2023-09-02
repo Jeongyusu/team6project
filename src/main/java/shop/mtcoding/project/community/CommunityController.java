49d29f7333cdf7e53a46486cc68c1854fdc5281d
muaga <more9494@naver.com>
2023-09-02 23:53:24 +0900

    커뮤니티 목록 완료(재)

--- src/main/java/shop/mtcoding/project/community/CommunityController.java
+++ src/main/java/shop/mtcoding/project/community/CommunityController.java
@@ -1,11 +1,13 @@
 package shop.mtcoding.project.community;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import shop.mtcoding.project.user.User;
@@ -15,6 +17,36 @@ public class CommunityController {
 
     @Autowired
     private CommunityService communityService;
+
+    // comp_ 커뮤니티 홈 화면
+    @GetMapping("comp/community")
+    public String compCommunity(@RequestParam(defaultValue = "0") Integer page, String keyword, Model model) {
+        Page<Community> communityPG = null;
+        if (keyword == null || keyword.trim().isEmpty()) {
+            communityPG = communityService.게시물목록(page);
+        } else {
+            communityPG = communityService.검색후게시물목록(page, keyword);
+        }
+        model.addAttribute("communityPG", communityPG);
+        model.addAttribute("prev", communityPG.getNumber() - 1);
+        model.addAttribute("next", communityPG.getNumber() + 1);
+        return "comp/comp_community_list";
+    }
+
+    // user_ 커뮤니티 홈 화면
+    @GetMapping("user/community")
+    public String userCommunity(@RequestParam(defaultValue = "0") Integer page, String keyword, Model model) {
+        Page<Community> communityPG = null;
+        if (keyword == null || keyword.trim().isEmpty()) {
+            communityPG = communityService.게시물목록(page);
+        } else {
+            communityPG = communityService.검색후게시물목록(page, keyword);
+        }
+        model.addAttribute("communityPG", communityPG);
+        model.addAttribute("prev", communityPG.getNumber() - 1);
+        model.addAttribute("next", communityPG.getNumber() + 1);
+        return "user/user_community_list";
+    }
 
     // comp_ 커뮤니티 글 등록 화면
     @GetMapping("comp/community/board/saveForm")
