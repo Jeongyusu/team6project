7282a367baa1217a9085d2470cda15f1b13a946c
Jeongyusu <onlys2545@naver.com>
2023-09-15 18:22:22 +0900

    배포 작업 전 세팅

--- src/main/java/shop/mtcoding/project/_core/error/MyExceptionHandler.java
+++ src/main/java/shop/mtcoding/project/_core/error/MyExceptionHandler.java
@@ -3,22 +3,27 @@ package shop.mtcoding.project._core.error;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import lombok.extern.slf4j.Slf4j;
 import shop.mtcoding.project._core.error.ex.MyApiException;
 import shop.mtcoding.project._core.error.ex.MyException;
 import shop.mtcoding.project._core.util.ApiUtil;
 import shop.mtcoding.project._core.util.Script;
 
+@Slf4j
 @RestControllerAdvice
 public class MyExceptionHandler {
 
     @ExceptionHandler(MyException.class)
     public String error(MyException e) {
+        log.debug("에러 " + e.getMessage());
+        log.info("에러 " + e.getMessage());
         return Script.back(e.getMessage());
     }
 
-
     @ExceptionHandler(MyApiException.class)
     public ApiUtil<String> error(MyApiException e) {
+        log.debug("에러 " + e.getMessage());
+        log.info("에러 " + e.getMessage());
         return new ApiUtil<>(false, e.getMessage());
     }
 }
